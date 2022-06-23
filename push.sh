03720c5a7dd72de492cbcabf27e69795078e0ffb
Jonggi Hong <jhong@ski.org>
2022-06-23 16:12:57 -0700

    mm github

--- push.sh
+++ push.sh
@@ -1,3 +1,5 @@
 git add --all
 git commit -m "mm github"
-git push https://github.com/jonggi/jonggi.github.io.git
+git push origin master
+
+# git remote set-url origin https://koreahjg1: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@github.com/jonggi/jonggi.github.io.git
